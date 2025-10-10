0c990875284a6387b3506a5a882e07941135b5f3
Amit <amitgupta561710@gmail.com>
2025-10-10 14:09:54 +0530

    ---

---
+++ 8.Dp/part_2/maxenergy.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maximumEnergy(vector<int>& energy, int k) {
+        int n = energy.size(), maxEnergy = INT_MIN;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            if (i + k < n) {
+                energy[i] += energy[i + k];
+            }
+            maxEnergy = max(maxEnergy, energy[i]);
+        }
+        
+        return maxEnergy;
+    }
+};
+
+int main() {
+
+    return 0;
+}
