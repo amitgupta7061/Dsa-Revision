ced1fc0c197c94624f21a81e02ab2ae18ff0f500
Amit <amitgupta561710@gmail.com>
2025-10-14 19:59:27 +0530

    ---

---
+++ 10.Slidingwindow/part_1/incsubarray.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+        int increasing = 1, prevIncreasing = 0;
+
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] > nums[i - 1]) ++increasing;
+            else {
+                prevIncreasing = increasing;
+                increasing = 1;
+            }
+            if (increasing / 2 >= k || min(prevIncreasing, increasing) >= k) return true;
+        }
+
+        return false;
+    }
+};
+
+int main() {
+
+    return 0;
+}
