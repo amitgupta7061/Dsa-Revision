f8cdf80defc58623e926985ee14d2f58efa458c0
Amit <amitgupta561710@gmail.com>
2025-10-24 09:33:57 +0530

    ---

---
+++ 12.Greedy/part_1/numbalanced.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int nextBeautifulNumber(int n) {
+        while (!isBalance(++n))
+        ;
+        return n;
+    }
+    bool isBalance(int num) {
+        vector<int> count(10);
+        while (num > 0) {
+            if (num % 10 == 0) return false;
+            ++count[num % 10];
+            num /= 10;
+        }
+        for (int i = 1; i < 10; ++i){
+            if (count[i] && count[i] != i) return false;
+        }
+        return true;
+    }
+};
+
+int main() {
+
+    return 0;
+}
