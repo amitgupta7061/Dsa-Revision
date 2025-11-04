ee5939d38d0a506d562101897d3f7a1658c62688
Amit <amitgupta561710@gmail.com>
2025-11-04 21:46:57 +0530

    ---

---
+++ 10.Slidingwindow/part_2/findxsum.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findXSum(vector<int>& nums, int k, int x) { 
+        set<pair<int, int>> topX, remaining; 
+        int xSum = 0; 
+      
+        unordered_map<int, int> frequency;
+        auto addElement = [&](int value) {
+            if (frequency[value] == 0) return;
+          
+            pair<int, int> element = {frequency[value], value};
+            if (!topX.empty() && element > *topX.begin()) {
+                xSum += element.first * element.second;  // Add to sum
+                topX.insert(element);
+            } else {
+                remaining.insert(element);
+            }
+        };
+      
+        auto removeElement = [&](int value) {
+            if (frequency[value] == 0) return;
+            pair<int, int> element = {frequency[value], value};
+          
+            auto it = topX.find(element);
+            if (it != topX.end()) {
+                xSum -= element.first * element.second; 
+                topX.erase(it);
+            } else {
+                remaining.erase(element);
+            }
+        };
+      
+        vector<int> result;
+      
+        for (int i = 0; i < nums.size(); ++i) {
+            removeElement(nums[i]);
+            ++frequency[nums[i]];
+            addElement(nums[i]);
+          
+            int windowStart = i - k + 1;
+            if (windowStart < 0) continue;
+          
+            while (!remaining.empty() && topX.size() < x) {
+                pair<int, int> largestRemaining = *remaining.rbegin();
+                xSum += largestRemaining.first * largestRemaining.second;
+                remaining.erase(largestRemaining);
+                topX.insert(largestRemaining);
+            }
+          
+            while (topX.size() > x) {
+                pair<int, int> smallestTop = *topX.begin();
+                xSum -= smallestTop.first * smallestTop.second;
+                topX.erase(smallestTop);
+                remaining.insert(smallestTop);
+            }
+          
+            result.push_back(xSum);
+          
+            removeElement(nums[windowStart]);
+            --frequency[nums[windowStart]];
+            addElement(nums[windowStart]);
+        }
+      
+        return result;
+    }
+};
+
+int main() {
+
+    return 0;
+}
