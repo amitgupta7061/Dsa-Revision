ded861a6b5cbf47c175d53ec3355042a86b75857
Amit Kumar Gupta <amitgupta561710@gmail.com>
2025-11-27 19:17:58 +0530

    ---

--- 14.prefixsum/part_1/subsumlenk.cpp
+++ 14.prefixsum/part_1/subsumlenk.cpp
@@ -4,9 +4,26 @@ using namespace std;
 class Solution {
 public:
     long long maxSubarraySum(vector<int>& nums, int k) {
-        
+        int n = nums.size();
+        vector<long long> prefsum(n+1, 0), best(k, LLONG_MAX);
+
+        for (int i = 0; i < n; i++) prefsum[i+1] = prefsum[i] + nums[i];
+
+        long long result = LLONG_MIN;
+
+        for (int i = 0; i <= n; i++) {
+            int r = i % k;
+
+            if (best[r] != LLONG_MAX) {
+                result = max(result, prefsum[i] - best[r]);
+            }
+            best[r] = min(best[r], prefsum[i]);
+        }
+
+        return result;
     }
 };
+
 
 
 int main() {
