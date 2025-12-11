73a9939b5e6edb4166d73ad4e1df34c8c1e8079f
Amit Kumar Gupta <amitgupta561710@gmail.com>
2025-12-11 21:45:32 +0530

    ----

--- codef/a.cpp
+++ codef/a.cpp
@@ -1,34 +1,23 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
     int t;
     cin >> t;
     while (t--) {
         int n;
         cin >> n;
-        set<int> colors;
-        for (int i = 0; i < n; i++) {
-            int a;
-            cin >> a;
-            colors.insert(a);
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
         }
-        while (true) {
-            int distinctCount = colors.size();
-            if (colors.count(distinctCount)) {
-                cout << distinctCount << "\n";
-                break;
-            }
-            colors.insert(distinctCount);
-            if (colors.size() > 2000) {
-                cout << distinctCount << "\n";
-                break;
-            }
+        int cnt = 0, maxi = 0;
+        for (int x : a) {
+            if (x < maxi) cnt++;
+            else maxi = x;
         }
+
+        cout<<cnt<<endl;
     }
-    
     return 0;
 }
--- codef/b.cpp
+++ codef/b.cpp
@@ -1,23 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int t;
     cin >> t;
     while (t--) {
-        int n, l, r;
-        cin >> n >> l >> r;
-        vector<int> p(n + 1);
-        iota(p.begin(), p.end(), 0); 
-        p[r] = p[l - 1];
-        for (int i = 1; i <= n; ++i) {
-            cout << (p[i] ^ p[i - 1]) << (i == n ? "" : " ");
+        int n;
+        string s;
+        cin >> n >> s;
+        vector<int> pos;
+        for (int i = 0; i < n; ++i)
+            if (s[i] == '1') pos.push_back(i);
+        if (pos.size() == n) {
+            cout << 0 << '\n';
+            continue;
         }
-        cout << "\n";
+        int m = pos.size(), ans = 0;
+        for (int i = 0; i < m; ++i) {
+            int j = (i + 1) % m;
+            int gap = (pos[j] - pos[i] - 1 + n) % n;
+            ans = max(ans, gap);
+        }
+        cout << ans << '\n';
     }
-    
     return 0;
 }
